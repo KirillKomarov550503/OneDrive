612263979c9133dbcab591f4637ed81a250f207e
KirillKomarov550503 <kirya.komarov.97@list.ru>
2018-11-25 20:58:05 +0300

    add repositories

--- Application/person-service/src/main/java/com/komarov/onedrive/dao/repository/CredentialRepository.java
+++ Application/person-service/src/main/java/com/komarov/onedrive/dao/repository/CredentialRepository.java
@@ -2,7 +2,9 @@ package com.komarov.onedrive.dao.repository;
 
 import com.komarov.onedrive.dao.entity.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CredentialRepository extends JpaRepository<Credential, Long> {
   Credential findCredentialByEmailAndAndPassword(String email, String password);
 }
--- Application/person-service/src/main/java/com/komarov/onedrive/dao/repository/PersonRepository.java
+++ Application/person-service/src/main/java/com/komarov/onedrive/dao/repository/PersonRepository.java
@@ -2,7 +2,9 @@ package com.komarov.onedrive.dao.repository;
 
 import com.komarov.onedrive.dao.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
